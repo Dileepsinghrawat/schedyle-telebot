4ca781eaec01479b01d0dfe361e72b8bfccae645
Dileepsinghrawat <87635025+Dileepsinghrawat@users.noreply.github.com>
2022-01-27 22:52:53 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,28 @@ bot = telebot.TeleBot(token)
 timers = ['-10', '+10', '-30', '+30']
 
 
+def job_exists(text):
+  a = schedule.jobs
+  for job in a:
+    if job.job_func.args[0].args[0] == text:
+      return True
+  return False
+
+def create_task(message, timer, edit=False):
+  markup = types.InlineKeyboardMarkup(row_width=4)
+  markup.row(
+    types.InlineKeyboardButton('-30', callback_data='-30'),
+    types.InlineKeyboardButton('-10', callback_data='-10'),
+    types.InlineKeyboardButton('+10', callback_data='+10'),
+    types.InlineKeyboardButton('+30', callback_data='+30')
+    )
+  markup.add(types.InlineKeyboardButton('✅ Schedule 🕒', callback_data='schedule'))
+  if edit:
+    bot.edit_message_text('*Set up time scheduler in minutes:*\nPost every {} min'.format(timer), message.chat.id,message.message_id, parse_mode='Markdown', reply_markup=markup)
+  else:
+    bot.reply_to(message, '*Set up time scheduler in minutes:*\nPost every {} min'.format(timer),parse_mode='Markdown',reply_markup=markup)
+
+
 @bot.message_handler(commands=['start'])
 def start_command(message):
   bot.send_message(message.chat.id, '<b>Hello {}</b>\n'.format(message.from_user.first_name),parse_mode='HTML')
