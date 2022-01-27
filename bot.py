aec8580134ebf77ffd54ca91fd5933ff5adbe3b6
Dileepsinghrawat <87635025+Dileepsinghrawat@users.noreply.github.com>
2022-01-27 22:38:46 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -16,4 +16,10 @@ timers = ['-10', '+10', '-30', '+30']
 def start_command(message):
   bot.send_message(message.chat.id, '<b>Hello {}</b>\n'.format(message.from_user.first_name),parse_mode='HTML')
 
+
+@bot.message_handler(commands=['add'])
+def post_command(message):
+  markup = types.ForceReply(selective=False)
+  bot.send_message(message.chat.id, 'Reply me with message you want to schedule', reply_markup=markup)
+
 bot.polling(none_stop=True)
